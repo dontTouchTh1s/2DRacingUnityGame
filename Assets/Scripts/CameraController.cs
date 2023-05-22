d6b883dea8942e50bcda8a1c806b8e44d87162ea
Ali Mashayekhi <alim201381@gmail.com>
2023-05-22 11:41:53 +0330

    Camera speed will increase if target reach its max speed

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,14 +5,15 @@ public class CameraController : MonoBehaviour
 {
     [CanBeNull] public GameObject carGameObject;
 
-    public readonly float cameraFirstMaxSpeedFactor = 8f;
-    private float _cameraMaxSpeedFactor;
+    public float cameraSpeedFactor = 0.1f;
+    private float _cameraSpeedFactor;
     private CarController _carController;
+
 
     // Start is called before the first frame update
     private void Start()
     {
-        _cameraMaxSpeedFactor = cameraFirstMaxSpeedFactor;
+        _cameraSpeedFactor = cameraSpeedFactor;
         _carController = carGameObject.GetComponent<CarController>();
     }
 
@@ -20,21 +21,17 @@ public class CameraController : MonoBehaviour
     private void Update()
     {
         if (!carGameObject) return;
-        var cameraSpeedFactor = Mathf.Lerp(0.5f, _carController.VelocityMagnitude / _cameraMaxSpeedFactor,
-            0.35f);
-        Debug.Log(_carController.maxSpeed / cameraFirstMaxSpeedFactor * 0.35f + 0.3f -
-                  cameraSpeedFactor);
-        if (cameraSpeedFactor >= _carController.maxSpeed / cameraFirstMaxSpeedFactor * 0.35f + 0.3f)
-            _cameraMaxSpeedFactor =
-                Mathf.Lerp(_cameraMaxSpeedFactor, cameraFirstMaxSpeedFactor - 6, 0.003f);
-        else
-            _cameraMaxSpeedFactor = cameraFirstMaxSpeedFactor;
-        Debug.Log(_cameraMaxSpeedFactor);
-
         var cameraPosition = transform.position;
         var carPosition = carGameObject.transform.position;
         var carXYPositionVector = new Vector3(carPosition.x, carPosition.y, cameraPosition.z);
+
+        var cameraSpeed = _carController.VelocityMagnitude * _cameraSpeedFactor + 0.5f;
+        if (cameraSpeed >= _carController.maxSpeed * _cameraSpeedFactor + 0.25f)
+            _cameraSpeedFactor = Mathf.Lerp(_cameraSpeedFactor, cameraSpeedFactor * 8f, 0.0005f);
+        else
+            _cameraSpeedFactor = Mathf.Lerp(_cameraSpeedFactor, cameraSpeedFactor, 0.0005f);
         transform.position = Vector3.Lerp(cameraPosition, carXYPositionVector,
-            Time.deltaTime * cameraSpeedFactor);
+            Time.deltaTime * cameraSpeed);
+        // transform.position = Vector3.SmoothDamp(cameraPosition, carXYPositionVector, ref _speed, 0.2f);
     }
 }
