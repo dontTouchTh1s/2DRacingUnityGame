243d798bd561a68f30c719c6d065a686a6291f8e
Ali Mashayekhi <alim201381@gmail.com>
2023-05-22 11:40:00 +0330

    Add guardRails with shadows and sprite shape

---
+++ Assets/Scripts/GuardRailShadowHandler.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardRailShadowHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private SpriteRenderer _spriteRenderer;
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
